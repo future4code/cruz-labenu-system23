c9f78cbdc1c763dccb2ef64c9df8ef1b2887f0ac
Nicolas <nicolasalexandre0001@gmail.com>
2021-06-04 16:18:34 -0300

    endpoints

--- commands.sql
+++ commands.sql
@@ -11,18 +11,14 @@ CREATE TABLE student (
   id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
   nome VARCHAR(50) NOT NULL,
   email VARCHAR(50) NOT NULL,
-  data_nasc DATE NOT NULL,
-  turma_id INT NOT NULL,
-  FOREIGN KEY (turma_id) REFERENCES class(id)
+  data_nasc DATE NOT NULL
 );
 
 CREATE TABLE teacher (
   id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
   nome VARCHAR(50) NOT NULL,
   email VARCHAR(50) NOT NULL,
-  data_nasc DATE NOT NULL,
-  turma_id INT NOT NULL,
-  FOREIGN KEY (turma_id) REFERENCES class(id)
+  data_nasc DATE NOT NULL
 );
 
 CREATE TABLE hobbie (
@@ -43,7 +39,37 @@ CREATE TABLE specialty (
   ) NOT NULL
 );
 
--- INSERINDO VALORES
+CREATE TABLE class_students (
+  turma_id INT NOT NULL,
+  estudante_id INT NOT NULL PRIMARY KEY,
+  FOREIGN KEY (turma_id) REFERENCES class(id),
+  FOREIGN KEY (estudante_id) REFERENCES student(id)
+);
+
+CREATE TABLE class_teachers (
+  turma_id INT NOT NULL,
+  professor_id INT NOT NULL,
+  CONSTRAINT class_teachers PRIMARY KEY (turma_id, professor_id),
+  FOREIGN KEY (turma_id) REFERENCES class(id),
+  FOREIGN KEY (professor_id) REFERENCES teacher(id)
+
+);
+
+-- ADICIONANDO ALUNOS EM TURMAS
+INSERT INTO
+  class_students (turma_id, estudante_id)
+VALUES
+  (1, 1),
+  (1, 2);
+
+-- ADICIONANDO PROFESSORES EM TURMAS
+INSERT INTO
+  class_teachers (turma_id, professor_id)
+VALUES
+  (1, 1),
+  (2, 1),
+  (1, 2);
+
 INSERT INTO
   class(nome, data_inicio, data_final, modulo)
 VALUES
@@ -53,41 +79,72 @@ VALUES
     '2020-08-01',
     6
   ),
-  (
-    'Cruz',
-    '2021-03-01',
+    (
+      'Cruz',
+      '2021-03-01',
     '2021-08-01',
     4
   );
 
+-- CRIANDO ALUNOS
 INSERT INTO
-  student(nome, email, data_nasc, turma_id)
+  student(nome, email, data_nasc)
 VALUES
   (
-    'Nicolas',
-    'nicolas@nicolas.com',
-    '2002-07-22',
-    1
+    'Marcelino',
+    'marcelino@hack.com',
+    '1990-01-31'
   ),
   (
-    'Janis',
-    'janis@janis.com',
-    '1998-05-09',
-    1
+    'Lucas',
+    'lucas@ehavida.com',
+    '1990-01-31'
   );
 
+-- CRIANDO PROFESSORES
 INSERT INTO
-  teacher(nome, email, data_nasc, turma_id)
+  teacher(nome, email, data_nasc)
 VALUES
   (
     'João Alves',
     'jão@labenu.com',
-    '1995-01-01',
-    1
+    '1995-01-01'
   ),
-   (
+  (
     'Matheus Gesualdo',
     'matt@labenu.com',
-    '1995-01-01',
-    2
+    '1995-01-01'
   );
+
+-- SELECIONANDO TODOS ESTUDANTES DE UMA TURMA
+SELECT
+  student.nome,
+  class.nome as turma
+FROM
+  class_students
+  JOIN student
+  JOIN class
+WHERE
+  class.id = 1
+  AND class_students.turma_id = 1
+  AND class_students.estudante_id = student.id;
+
+
+-- SELECIONANDO TODOS PROFESSORES DE UMA TURMA
+SELECT
+  teacher.nome,
+  class.nome as turma
+FROM
+  class_teachers
+  JOIN teacher
+  JOIN class
+WHERE
+  class.id = 1
+  AND class_teachers.turma_id = 1
+  AND class_teachers.professor_id = teacher.id;
+
+
+-- CRIAR TURMA
+CREATE TABLE class (nome, data_inicio, data_final,)
+VALUES
+  ('Nova turma', '2021-06-04', '2021-12-04');
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,206 @@
 import { Request, Response } from "express";
 import app from "./app";
 import connection from "./connection";
+import moment from "moment";
 
-app.get('/ping', (req: Request, res: Response)=>{
-  res.send('pong')
-})
+//string to data moment("22/07/2002", "DD/MM/YYYY").format('YYYY-MM-DD');
+//data to string moment("2002-07-22").format('DD/MM/YYYY');
+
+app.put("/teacher/create", async (req: Request, res: Response) => {
+  try {
+    if (!req.body.nome) {
+      throw Error("Está faltando o campo 'nome' no body!");
+    }
+    if (!req.body.email) {
+      throw Error("Está faltando o campo 'email' no body!");
+    }
+    if (!req.body.dataNasc) {
+      throw Error("Está faltando o campo 'dataNasc' no body!");
+    }
+    await connection.raw(`
+    INSERT INTO teacher(nome, email, data_nasc)
+    VALUES
+    (
+      '${req.body.nome}',
+      '${req.body.email}',
+      '${moment(req.body.dataNasc, "DD/MM/YYYY").format("YYYY-MM-DD")}'
+    );
+    `);
+    res
+      .status(200)
+      .send({ message: "Professor adicionado no sistema com sucesso!" });
+  } catch (error) {
+    res.status(500).send({ message: error.message || error.sqlMessage });
+  }
+});
+
+app.put("/student/create", async (req: Request, res: Response) => {
+  try {
+    if (!req.body.nome) {
+      throw Error("Está faltando o campo 'nome' no body!");
+    }
+    if (!req.body.email) {
+      throw Error("Está faltando o campo 'email' no body!");
+    }
+    if (!req.body.dataNasc) {
+      throw Error("Está faltando o campo 'dataNasc' no body!");
+    }
+
+    await connection.raw(`
+    INSERT INTO student(nome, email, data_nasc)
+    VALUES
+    (
+      "${req.body.nome}",
+      "${req.body.email}",
+      "${moment(req.body.dataNasc, "DD/MM/YYYY").format("YYYY-MM-DD")}"
+    )
+    `);
+    res
+      .status(200)
+      .send({ message: "Aluno adicionado no sistema com sucesso!" });
+  } catch (error) {
+    res.status(500).send({ message: error.message || error.sqlMessage });
+  }
+});
+
+app.put("/class/create", async (req: Request, res: Response) => {
+  try {
+    if (!req.body.nome) {
+      throw Error("Por favor, digite um nome");
+    }
+    if (!req.body.dataInicio) {
+      throw Error("Por favor, digite a data inicial do curso");
+    }
+    if (!req.body.dataFinal) {
+      throw Error("Por favor, digite a data final do curso");
+    }
+
+    await connection.raw(`
+    INSERT INTO class(nome, data_inicio, data_final)
+    VALUES
+    (
+      "${req.body.nome}",
+      "${moment(req.body.dataInicio, "DD/MM/YYYY").format("YYYY-MM-DD")}",
+      "${moment(req.body.dataFinal, "DD/MM/YYYY").format("YYYY-MM-DD")}"
+    );
+    `);
+    res.status(200).send({ message: "Turma adicionada com sucesso!" });
+  } catch (error) {
+    res.status(500).send({ message: error.message || error.sqlMessage });
+  }
+});
+
+app.put("/class/addTeacher", async (req: Request, res: Response) => {
+  try {
+    if (!req.body.teacherID) {
+      throw Error("Por favor, preencha o campo teacherID");
+    }
+    if (!req.body.classID) {
+      throw Error("Por favor, preencha o campo classID");
+    }
+    await connection.raw(`
+    INSERT INTO
+      class_teachers (turma_id, professor_id)
+    VALUES
+      (${req.body.classID}, ${req.body.teacherID});`);
+    res.status(200).send({ message: "Professor adicionado com sucesso!" });
+  } catch (error) {
+    if (
+      error.sqlMessage &&
+      error.sqlMessage.includes("foreign key constraint fails")
+    ) {
+      res.status(500).send({ message: "Professor ou turma não encontrados" });
+    }
+    if (error.sqlMessage && error.sqlMessage.includes("Duplicate")) {
+      res.status(500).send({ message: "Esse professor já está nessa turma!" });
+    }
+    res.status(500).send({ message: error.message || error.sqlMessage });
+  }
+});
+app.put("/class/addStudent", async (req: Request, res: Response) => {
+  try {
+    if (!req.body.studentID) {
+      throw Error("Por favor, preencha o campo studentID");
+    }
+    if (!req.body.classID) {
+      throw Error("Por favor, preencha o campo classID");
+    }
+    await connection.raw(`
+    INSERT INTO
+      class_students (turma_id, estudante_id)
+    VALUES
+      (${req.body.classID}, ${req.body.studentID});`);
+    res.status(200).send({ message: "Aluno adicionado com sucesso!" });
+  } catch (error) {
+    if (error.sqlMessage && error.sqlMessage.includes("Duplicate")) {
+      res.status(500).send({ message: "Esse estudante já está em uma turma!" });
+    }
+    if (
+      error.sqlMessage &&
+      error.sqlMessage.includes("foreign key constraint fails")
+    ) {
+      res.status(500).send({ message: "Estudante ou turma não encontrados" });
+    }
+    res.status(500).send({ message: error.message || error.sqlMessage });
+  }
+});
+
+app.get("/student/age/:id", async (req: Request, res: Response) => {
+  try {
+    const id = Number(req.params.id);
+    const [result] = await connection.raw(`
+      SELECT student.data_nasc FROM student WHERE student.id=${id};
+    `);
+    if (!result.length) {
+      throw Error("Estudante não encontrado!");
+    }
+    const idade = Number(
+      moment(result[0].data_nasc, "YYYY-MM-DD").fromNow().replace(/\D/g, "")
+    );
+    res.status(200).send({ dataNascimento: idade });
+  } catch (error) {
+    res.status(500).send({ message: error.message || error.sqlMessage });
+  }
+});
+
+app.get("/student/class/:id", async (req: Request, res: Response) => {
+  try {
+    const [result] = await connection.raw(`
+    SELECT
+      student.nome,
+      class.nome as turma
+    FROM
+      class_students
+    JOIN student
+    JOIN class
+    WHERE
+      class.id = ${req.params.id}
+      AND class_students.turma_id = ${req.params.id}
+      AND class_students.estudante_id = student.id;
+    `);
+    res.status(200).send(result);
+  } catch (error) {
+    res.status(500).send({ message: error.message || error.sqlMessage });
+  }
+});
+
+app.get("/teacher/class/:id", async (req: Request, res: Response) => {
+  try {
+    const [result] = await connection.raw(`
+      SELECT
+        teacher.nome,
+        class.nome as turma
+      FROM
+        class_teachers
+        JOIN teacher
+        JOIN class
+      WHERE
+        class.id = ${req.params.id}
+        AND class_teachers.turma_id = ${req.params.id}
+        AND class_teachers.professor_id = teacher.id;
+        `);
+      res.status(200).send(result);
+  } catch (error) {
+    res.status(500).send({ message: error.message || error.sqlMessage });
+  }
+});
